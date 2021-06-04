f8bfdae864050101652f7d5cb3fe048bc32f6ecd
akshar001 <vastarparaakshar@gmail.com>
2021-06-04 20:05:32 +0530

    first

--- main/BNO080.cpp
+++ main/BNO080.cpp
@@ -41,8 +41,120 @@
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
 
 #include "BNO080.h"
+
+
+#define SAMPLE_PERIOD_MS        200
+
+#define I2C_SCL_IO              22  //19               /*!< gpio number for I2C master clock */
+#define I2C_SDA_IO              21  //18               /*!< gpio number for I2C master data  */
+#define I2C_FREQ_HZ             100000           /*!< I2C master clock frequency */
+#define I2C_PORT_NUM            I2C_NUM_1        /*!< I2C port number for master dev */
+#define I2C_TX_BUF_DISABLE      0                /*!< I2C master do not need buffer */
+#define I2C_RX_BUF_DISABLE      0                /*!< I2C master do not need buffer */
+
+// I2C common protocol defines
+#define WRITE_BIT                          I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT                           I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
+#define ACK_VAL                            0x0              /*!< I2C ack value */
+
+
+#define NACK_VAL                           0x1              /*!< I2C nack value */
+
+static const char *TAG = "i2c_restart";
+
+#define BNO080_I2C_ADDR 0x12
+
+
+static esp_err_t i2c_master_read_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_rd, size_t size)
+{
+    if (size == 0) {
+        return ESP_OK;
+    }
+    
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, i2c_addr << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, i2c_reg, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret != ESP_OK) {
+        ESP_LOGI(TAG, "!ESP_OK 1");
+        return ret;
+    }
+    vTaskDelay(30 / portTICK_RATE_MS);
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, i2c_addr << 1 | READ_BIT, ACK_CHECK_EN);
+    // i2c_master_read_byte(cmd, data_h, ACK_VAL);
+    i2c_master_read_byte(cmd, data_rd, NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+
+static esp_err_t i2c_master_write_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    // first, send device address (indicating write) & register to be written
+    i2c_master_write_byte(cmd, ( i2c_addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    // send register we want
+    i2c_master_write_byte(cmd, i2c_reg, ACK_CHECK_EN);
+    // write the data
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+
+/* Read contents of a MX register
+---------------------------------------------------------------------------*/
+esp_err_t rdBNO80( uint8_t reg, uint8_t *pdata, uint8_t count )
+{
+    return ( i2c_master_read_slave_reg( I2C_PORT_NUM, BNO080_I2C_ADDR,  reg, pdata, count ) );
+}
+
+/* Write value to specified MMA8451 register
+---------------------------------------------------------------------------*/
+esp_err_t wrBNO80( uint8_t reg, uint8_t *pdata, uint8_t count )
+{
+    return ( i2c_master_write_slave_reg( I2C_PORT_NUM, BNO080_I2C_ADDR,  reg, pdata, count ) );
+}
+
+static void i2c_master_init()
+{
+    int i2c_master_port = I2C_PORT_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_FREQ_HZ;
+    conf.clk_flags = 0;
+    // i2c_param_config(i2c_master_port, &conf);
+
+    esp_err_t err = i2c_param_config(I2C_PORT_NUM, &conf);
+    if (err != ESP_OK) {
+        ESP_LOGE( TAG , "Driver is not being initialised");
+        return err;
+    }
+    i2c_driver_install(i2c_master_port, conf.mode,
+            I2C_RX_BUF_DISABLE, I2C_TX_BUF_DISABLE, 0);
+}
+
 
 //CTOR DTOR already implemented in .h
 
@@ -791,7 +903,6 @@ bool BNO080::receivePacket(void)
 bool BNO080::getData(uint16_t bytesRemaining)
 {
     uint16_t dataSpot = 0; //Start at the beginning of shtpData array
-
     //Setup a series of chunked 32 byte reads
     while (bytesRemaining > 0)
     {
@@ -800,13 +911,11 @@ bool BNO080::getData(uint16_t bytesRemaining)
 
         // i2c->write_byte(0, (uint8_t)(numberOfBytesToRead + 4));
         if (waitForI2C() == false) return (0); //Error
-
         //The first four bytes are header bytes and are throw away
         // i2c->read_only();
         // i2c->read_only();
         // i2c->read_only();
         // i2c->read_only();
-
         for (uint8_t x = 0 ; x < numberOfBytesToRead ; x++)
         {
             // uint8_t incoming = i2c->read_only();
